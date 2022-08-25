8e738f41c2f56c522fb4afacf8425dd7717817cd
Camilo Espinosa <camesp96@gmail.com>
2022-08-24 21:40:43 -0600

    Finish jumpscare base class and one jumpscare child class

---
+++ Source/HorrorGame/Private/Actors/JumpscareActors/DoorwayJumpscare.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/JumpscareActors/DoorwayJumpscare.h"
+//#include "Animation/AnimationAsset.h"
+#include "Components/ArrowComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+ADoorwayJumpscare::ADoorwayJumpscare()
+{
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(GetRootComponent());
+	Mesh->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+
+	EndPoint = CreateDefaultSubobject<UArrowComponent>(TEXT("End Point"));
+	EndPoint->SetupAttachment(GetRootComponent());
+	EndPoint->SetRelativeLocation(FVector(500.f, 0.f, 0.f));
+
+	bCalledJumpscare = false;
+	AnimationInterpSpeed = 300.f;
+	SecondsToDestroyThisEvent = 1.f;
+	
+	//SetActorTickEnabled(false);
+}
+
+void ADoorwayJumpscare::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADoorwayJumpscare::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+}
+
+void ADoorwayJumpscare::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bCalledJumpscare)
+	{
+		FVector CurrentValue = Mesh->GetComponentLocation();
+		FVector Target = EndPoint->GetComponentLocation();
+		FVector NewLocation = UKismetMathLibrary::VInterpTo_Constant(CurrentValue, Target, DeltaTime, AnimationInterpSpeed);
+
+		Mesh->SetWorldLocation(NewLocation);
+	}
+}
+
+void ADoorwayJumpscare::TriggerJumpscare(AActor* OverlappingActor)
+{
+	Super::TriggerJumpscare(OverlappingActor);
+
+	bCalledJumpscare = true;
+	/*
+	* OPTIONAL:
+	* - Disable movement.
+	* - Force character face the event direction.
+	*/
+
+	FTimerHandle DestroyTriggerBoxTimer;
+	GetWorld()->GetTimerManager().SetTimer(DestroyTriggerBoxTimer, this, &ADoorwayJumpscare::DestroyJumpscare, SecondsToDestroyThisEvent, false);
+}
+
+void ADoorwayJumpscare::DestroyJumpscare()
+{
+	Destroy();
+}
---
+++ Source/HorrorGame/Public/Actors/JumpscareActors/DoorwayJumpscare.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/JumpscareActors/JumpscareMaster.h"
+#include "DoorwayJumpscare.generated.h"
+
+class UArrowComponent;
+//class AnimationAsset;
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API ADoorwayJumpscare : public AJumpscareMaster
+{
+	GENERATED_BODY()
+
+public:
+
+	ADoorwayJumpscare();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void TriggerJumpscare(AActor* OverlappingActor) override;
+
+private:
+
+	/* Mesh for the monster that will scare the player */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Jumpscare")
+	USkeletalMeshComponent* Mesh;
+
+	/* Point where the mesh stops moving */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UArrowComponent* EndPoint;
+
+	bool bCalledJumpscare;
+
+	/* Speed for the animation to play when scaring */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Jumpscare")
+	float AnimationInterpSpeed;
+
+	/* Time in seconds to destroy this event */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Jumpscare")
+	float SecondsToDestroyThisEvent;
+
+	///* Animation our mesh will play when scaring the player */
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Jumpscare")
+	//UAnimationAsset* AnimationToPlay;
+
+protected:
+
+	/* Destroy this jumpscare event after being triggered */
+	UFUNCTION()
+	void DestroyJumpscare();
+	
+};
