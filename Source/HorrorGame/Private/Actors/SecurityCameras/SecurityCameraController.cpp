f4e2dbf06f5cd1db8a61353ea90b4be8843bf1cb
Camilo Espinosa <camesp96@gmail.com>
2022-08-29 20:00:16 -0600

    More camera changes

--- Source/HorrorGame/Private/Actors/SecurityCameras/SecurityCameraController.cpp
+++ Source/HorrorGame/Private/Actors/SecurityCameras/SecurityCameraController.cpp
@@ -65,8 +65,8 @@ void ASecurityCameraController::LeaveCameraController()
 
 	// Removing bindings
 	InteractingPlayer->OnJumpButtonPressed.RemoveDynamic(this, &ASecurityCameraController::ViewThroughCamera);
-	//InteractingPlayer->OnInteractButtonPressed.RemoveDynamic(this, &ASecurityCameraController::LeaveCameraController);
 	InteractingPlayer->OnInventoryButtonPressed.RemoveDynamic(this, &ASecurityCameraController::LeaveCameraController);
+	InteractingPlayer->OnReturnButtonPressed.RemoveDynamic(this, &ASecurityCameraController::LeaveCameraController);
 
 	InteractingPlayerController->SetViewTargetWithBlend(InteractingPlayer);
 
@@ -104,8 +104,8 @@ void ASecurityCameraController::Interact()
 			// Change camera view binding.
 			InteractingPlayer->OnJumpButtonPressed.AddDynamic(this, &ASecurityCameraController::ViewThroughCamera);
 			// Exit SecurityCameraController binding
-			//InteractingPlayer->OnInteractButtonPressed.AddDynamic(this, &ASecurityCameraController::LeaveCameraController);
 			InteractingPlayer->OnInventoryButtonPressed.AddDynamic(this, &ASecurityCameraController::LeaveCameraController);
+			InteractingPlayer->OnReturnButtonPressed.AddDynamic(this, &ASecurityCameraController::LeaveCameraController);
 
 			//InteractingPlayer->DisableInput(InteractingPlayerController); // ?
 			//InteractingPlayerController->SetIgnoreLookInput(true);
